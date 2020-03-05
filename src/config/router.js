e54aff864af1984547596f67da14efa0af4ea21d
Aleksandr Kisliy <akisliy2306#gmail.com>
2020-03-05 23:00:19 +0200

    refactor: removed business logic from the router

--- src/config/router.js
+++ src/config/router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const UserService = require('../components/User/service');
 const UserRouter = require('../components/User/router');
 
 module.exports = {
@@ -12,66 +11,6 @@ module.exports = {
      */
     init(app) {
         const router = express.Router();
-    
-        /**
-         * Renders user page to URL request /users.
-         * @name /users
-         * @function
-         * @inner
-         * @param {string} path - Express path
-         * @param {callback} middleware - Express middleware.
-         */
-        app.get('/users', async (req, res, next) => {
-            try {
-                const users = await UserService.findAll();
-
-                res.render('users', {users: users, popup: null});
-            } catch (error) {
-                res.render('users', {users: null, popup: null});
-
-                next(error);
-            }
-        });
-
-        /**
-         * Renders user page with popup-create.
-         * @name /users
-         * @function
-         * @inner
-         * @param {string} path - Express path
-         * @param {callback} middleware - Express middleware.
-         */
-        app.post('/users/create', async (req, res, next) => {
-            try {
-                const users = await UserService.findAll();
-
-                res.render('users', {users: users, popup: 'create'});
-            } catch (error) {
-                res.render('users', {users: null, popup: null});
-
-                next(error);
-            }
-        });
-
-        /**
-         * Renders user page with popup-update.
-         * @name /users
-         * @function
-         * @inner
-         * @param {string} path - Express path
-         * @param {callback} middleware - Express middleware.
-         */
-        app.post('/users/update', async (req, res, next) => {
-            try {
-                const users = await UserService.findAll();
-
-                res.render('users', {users: users, popup: 'update', id: req.body.id});
-            } catch (error) {
-                res.render('users', {users: null, popup: null});
-
-                next(error);
-            }
-        });
 
         /**
          * Forwards any requests to the /v1/users URI to UserRouter.
@@ -81,7 +20,17 @@ module.exports = {
          * @param {string} path - Express path
          * @param {callback} middleware - Express middleware.
          */
-        app.use('/v1/users', UserRouter);
+        app.use('/v1/users', UserRouter.router);
+
+        /**
+         * Forwards any requests to the /users URI to UserRouter.
+         * @name /users
+         * @function
+         * @inner
+         * @param {string} path - Express path
+         * @param {callback} middleware - Express middleware.
+         */
+        app.use('/users', UserRouter.viewRouter);
     
         /** 
          * @description No results returned mean the object is not found
